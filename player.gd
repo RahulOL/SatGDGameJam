47cbb1e40ca778f5b20a73f7630b2aa57c781220
RahulOL <106858149+RahulOL@users.noreply.github.com>
2022-06-11 11:59:35 +0600

    progress

---
+++ player.gd
@@ -0,0 +1,41 @@
+extends KinematicBody2D
+
+
+export (int) var speed = 700
+export (int) var jump_speed = 1200
+export (int) var gravity = 3000
+var velocity = Vector2.ZERO
+
+onready var animsprite = $AnimatedSprite
+
+func get_input():
+	velocity.x = 0
+	#Ugly Animation Handling :V
+	if velocity.y != 0:
+		animsprite.play("Jump")
+		if Input.is_action_pressed("ui_right"):
+				animsprite.set_flip_h(false)
+				velocity.x += speed
+		elif Input.is_action_pressed("ui_left"):
+				animsprite.set_flip_h(true)
+				velocity.x -= speed
+	else:
+		if Input.is_action_pressed("ui_right"):
+			animsprite.set_flip_h(false)
+			animsprite.play("Run")
+			velocity.x += speed
+		elif Input.is_action_pressed("ui_left"):
+			animsprite.set_flip_h(true)
+			animsprite.play("Run")
+			velocity.x -= speed
+		else:
+			animsprite.play("Idle")
+
+func _physics_process(delta):
+	get_input()
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+	if Input.is_action_just_pressed("ui_up"):
+		if is_on_floor():
+			velocity.y = -jump_speed
+
---
+++ src/Actors/Player.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+const GRAVITY = 10 # in pixels
+const JUMP = 50 # start speed px/s
+const SPEED = 100 # px/s
+
+var velocity = Vector2.ZERO # (0,0)
+
+func _ready():
+	pass
+	
+func _on_Timer_timeout():
+	print(str($Timer.wait_time) + " second(s) finished")
+	
+func _physics_process(delta):
+	
+	# if no keyboard input for left/right then x speed is 0
+	velocity.x = 0 
+	if(Input.is_action_pressed("ui_right")):
+		velocity.x = SPEED
+		
+		
+	elif(Input.is_action_pressed("ui_left")):
+		velocity.x = -SPEED
+	
+	velocity.y += GRAVITY
+	
+	if (Input.is_action_just_pressed("ui_up")):
+		velocity.y -= 25
+		$Timer.start(8) 
+		
+	velocity = move_and_slide(velocity)
